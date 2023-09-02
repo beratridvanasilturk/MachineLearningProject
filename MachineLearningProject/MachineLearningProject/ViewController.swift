1bdc49f6df4db91ef0bafbf4e1dcdbcdb66dbbc9
beratridvanasilturk <br.asltrk@gmail.com>
2023-09-03 01:34:53 +0300

    Added Completion Handler and ML Success Percent

--- MachineLearningProject/MachineLearningProject/ViewController.swift
+++ MachineLearningProject/MachineLearningProject/ViewController.swift
@@ -10,10 +10,10 @@ import CoreML
 import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
-
+    
     // CIImage kullanmanin amaci CoreMl'de import ettigimiz MobileNetV2 modulunde image olarak CIImage kullanilmasidir. Dokumantasyondan bakabilirsin ayrintilara
     var chosenImage = CIImage()
     
@@ -21,7 +21,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
     @IBAction func changeButtonTapped(_ sender: Any) {
         
         // picker ile image cektik galeriden veya kameradan
@@ -48,9 +48,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         recognizeImage(image: chosenImage)
     }
     // CI image kullanilarak image alinma islemi yapilir
+    // Bu fonksiyon icerisinde istek olusturup bunu handle edecegiz (yani ele alacagiz)
     func recognizeImage(image: CIImage) {
         
+        if let model = try? VNCoreMLModel(for: MobileNetV2().model) {
+                // Request
+            let request = VNCoreMLRequest(model: model) { (vnRequest, error) in
+                
+                // vnRequest kullanarak sonuclari alacagiz
+                // VNClassificationObservation: gorsel analizi sonucunda uretilen siniflandirma
+                if let results = vnRequest.results as? [VNClassificationObservation] {
+                    if results.count > 0 {
+                        // Ilk sonucu alarak en dogru secenegi bulacagimizi dusunuyoruz
+                        let topResult = results.first
+                        
+                        // Yapilan islemin %'sini verir Orn: %90 benzerlikle bu sonuca ulasildi gibi
+                        // Kullaniciya dispatch queue ile sonuclari gosterecegiz ve completion handler kullanacagiz
+                        DispatchQueue.main.async {
+                            // Tahmin edilen yuzde degeri
+                            let confidenceLevel = (topResult?.confidence ?? 0) * 100
+                            
+                            // Yuzde icin dogru cevrim gostermede kullandik
+                            
+                            self.resultLabel.text = "\(confidenceLevel)" + "%: \(topResult!.identifier) "
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            // Completion Handler
+            let handler = VNImageRequestHandler(ciImage: image)
+            DispatchQueue.global().async {
+                do {
+                
+                    try handler.perform([request])
+                } catch {
+                    print("Error in do catch while Handler")
+                }
+            }
+        }
     }
-    
 }
-
